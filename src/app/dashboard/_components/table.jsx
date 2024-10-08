2f0f478fa932ba1877fa72c8f1f315fc810407c3
Madesatya9904 <nightstoreindo@gmail.com>
2024-10-08 18:45:42 +0800

    fix code table in dasboard

--- src/app/dashboard/_components/table.jsx
+++ src/app/dashboard/_components/table.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import Cookies from "js-cookie";
 import { useRouter } from "next/navigation";
 import axios from "axios";
